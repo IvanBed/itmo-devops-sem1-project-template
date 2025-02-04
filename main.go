d2dae49e4d814ed3b151a6fff894498ed7f1ec6c
IvanBed <116827491+IvanBed@users.noreply.github.com>
2025-02-04 17:29:26 +0300

    (Fix) Добавление вызова функций БД в main.

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+	"project_sem/internal/db"
 	"project_sem/internal/handlers"
 )
 
 func main() {
 
+	db.InitDB()
 	http.HandleFunc("/api/v0/prices", handlers.Handler)
 
 	err := http.ListenAndServe(":8080", nil)
@@ -15,4 +17,5 @@ func main() {
 	if err != nil {
 		log.Println("Ошибка при запуске сервера:", err)
 	}
+	db.CloseDB()
 }
